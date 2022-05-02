506da896179dd6aef6714cd437e51c63638d2981
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2022-05-01 17:53:35 -0700

    initialize EmailDraft.java
    Created the class that will serve as the tool for creating emails

---
+++ src/EmailDraft.java
@@ -0,0 +1,52 @@
+public class EmailDraft {
+    String sender;
+    String recipients;
+    String subject;
+    String message;
+
+    /**
+     * Creates the draft of an email
+     * @param sender the sender email address
+     * @param recipients the recipients' email addresses, seperated by commas
+     * @param subject the subject of the email
+     * @param message the body of the message
+     */
+    public EmailDraft(String sender, String recipients, String subject, String message){
+        this.sender = sender;
+        this.recipients = recipients;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    /**
+     * Get method for the draft's sender address
+     * @return the address of the sender
+     */
+    public String getSender() {
+        return sender;
+    }
+
+    /**
+     * Get method for draft's recipients' addresses
+     * @return the address of the recipient
+     */
+    public String getRecipients() {
+        return recipients;
+    }
+
+    /**
+     * Get method for draft's subject
+     * @return the subject of the email
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * Get method for draft's test
+     * @return the message contained by the email
+     */
+    public String getMessage() {
+        return message;
+    }
+}
