01e16347370c3b1bd44c40bc4810893f8aab470c
ana569 <83257568+ana569@users.noreply.github.com>
2022-05-15 12:15:47 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+//https://youtu.be/jm_gVp12h0s?t=1223
+//need to create observable list to add data row to table
+//https://docs.oracle.com/javafx/2/ui_controls/table-view.htm
 public class Main extends Application {
+
+    public Button butInbox;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -15,8 +28,45 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    @FXML
+    private TableView<EmailMessage> emTable;
+
+    @FXML
+    private TableColumn<EmailMessage, String> emDate;
+
+    @FXML
+    private TableColumn<EmailMessage, String> emSubject;
+
+
+    @FXML
+    private TableColumn<EmailMessage, String> emFrom;
+
+
+    @FXML
+    private TableColumn<EmailMessage, ArrayList<Integer>> emTo;
+
+
+    @FXML
+    private TableColumn<EmailMessage, String> emMessage;
+
+
+    public void inbox(ActionEvent e){
+
+}
+public void compose(ActionEvent e){
+
+}
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void handle(MouseEvent mouseEvent) {
+    }
+
+    public void inboxButtClicked(MouseEvent mouseEvent) {
+    }
+
+    public void composeButtClicked(MouseEvent mouseEvent) {
     }
 }
